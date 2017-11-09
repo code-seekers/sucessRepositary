a154903c07f990a4666da665313a6ecf01cbc8f3
geethapothuraju <33508208+geethapothuraju@users.noreply.github.com>
2017-11-09 12:11:50 +0530

    Add files via upload

---
+++ SwitchExample.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class SwitchExample{
+	public static void main(String args[]){
+ 
+                System.out.println("Enter a number:");
+		Scanner s= new Scanner(System.in);
+		while(!s.hasNextInt()){
+			System.out.println("Invalid Input, Please Enter a Number:");
+			s.next();
+		}
+	        int n = s.nextInt();
+
+ 		System.out.println("Entered number is:"+n);
+		switch(n){
+			case 0: System.out.println("case 0 executed:");
+			break;
+			case 10: System.out.println("case 10 executed:");
+			break;
+			case 20: System.out.println("case 20 executed:");
+			break;
+			case 30: System.out.println("case 30 executed:");
+			break;
+			default: System.out.println("default executed:");
+			break;
+
+		}
+ 		
+	}
+}
+
